cadaf386c50e03a8fe0b1dc52549fe17a931553c
wsssz <53510251+wsssz@users.noreply.github.com>
2019-07-31 16:48:09 +0800

    Add files via upload

---
+++ CHANGEDB.php
@@ -0,0 +1,11 @@
+<?php
+//USE ;end TO SEPARATE SQL STATEMENTS. DON'T USE ;end IN ANY OTHER PLACES!
+
+$sql=array() ;
+$count=0 ;
+
+//v0.0.00
+$sql[$count][0]="0.0.00" ;
+$sql[$count][1]="-- First version, nothing to update" ;
+
+?>
